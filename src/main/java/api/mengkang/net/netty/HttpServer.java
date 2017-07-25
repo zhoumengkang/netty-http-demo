c437245e8b6b6a7d032a3e02fda37a0c540f8910
mengkang.zmk <mengkang.zmk@alibaba-inc.com>
2017-07-25 14:11:21 +0800

    第一步构架一个 web 服务器

---
+++ src/main/java/api/mengkang/net/netty/HttpServer.java
@@ -0,0 +1,39 @@
+package api.mengkang.net.netty;
+
+import api.mengkang.net.Config;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.handler.ssl.SslContext;
+
+/**
+ * @author zhoumengkang
+ */
+public final class HttpServer {
+
+    private static final int PORT = Config.getInt("server.port");
+
+    public static void main(String[] args) throws Exception {
+        final SslContext sslCtx = null;
+
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new HttpServerInitializer(sslCtx));
+
+            Channel ch = b.bind(PORT).sync().channel();
+            ch.closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
---
+++ src/main/java/api/mengkang/net/netty/HttpServerInitializer.java
@@ -0,0 +1,33 @@
+package api.mengkang.net.netty;
+
+/**
+ * Created by zhoumengkang on 16/6/23.
+ */
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.websocketx.extensions.compression.WebSocketServerCompressionHandler;
+import io.netty.handler.ssl.SslContext;
+
+public class HttpServerInitializer extends ChannelInitializer<SocketChannel> {
+
+    private final SslContext sslCtx;
+
+    public HttpServerInitializer(SslContext sslCtx) {
+        this.sslCtx = sslCtx;
+    }
+
+    @Override
+    public void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+        if (sslCtx != null) {
+            pipeline.addLast(sslCtx.newHandler(ch.alloc()));
+        }
+        pipeline.addLast(new HttpServerCodec());
+        pipeline.addLast(new HttpObjectAggregator(65536));
+        pipeline.addLast(new WebSocketServerCompressionHandler());
+        pipeline.addLast(new HttpServerHandler());
+    }
+}
